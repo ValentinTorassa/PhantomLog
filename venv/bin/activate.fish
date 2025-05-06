34c33121d862bb7e7d34f58d34eb142f644e46f7
Valen Torassa <valentintorassacolombero@gmail.com>
2025-05-06 15:09:21 -0300

    cambios front

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/valen/Downloads/PhantomLog/venv
+set -gx VIRTUAL_ENV /home/valen/Desktop/Programacion/PhantomLog/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
